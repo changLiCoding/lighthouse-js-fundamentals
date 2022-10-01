6e71dece60273742110748c97cc3813a5a84a1d3
javascriptsucks <evix1988@live.cn>
2022-10-01 23:30:36 +0800

    Add more conditional examples

--- 1.jsCondition.js
+++ 1.jsCondition.js
@@ -1,13 +1,11 @@
 const raining = true;
 const cold = false;
 
-if ( raining )
-{
+if ( raining ) {
   console.log( "Don't forget your umbrella!" );
 }
 
-if ( cold )
-{
+if ( cold ) {
   console.log( "Make sure you pick out a scarf!" );
 }
 
@@ -15,15 +13,28 @@ console.log( "Now you're ready to go outside!" );
 
 const temperature = 12;
 
-if ( temperature < 0 )
-{
+if ( temperature < 0 ) {
   console.log( "Make sure you pick out a scarf!" );
-} else if ( temperature < 15 )
-{
+} else if ( temperature < 15 ) {
   console.log( "Short sleeves won't cut it!" );
-} else
-{
+} else {
   console.log( "Short sleeves are fine." );
 }
 
 console.log( "Now you're ready to go outside!" );
+
+
+const isCitizen = true;
+const age = 26;
+
+if ( isCitizen && age > 18 ) {
+  console.log( "You are eligible to vote." );
+}
+
+if ( temperature < -40 || temperature > 40 ) {
+  console.log( "Maybe going outside isn't such a great idea…" );
+}
+
+if ( !raining ) {
+  console.log( "Leave your umbrella at home!" );
+}
